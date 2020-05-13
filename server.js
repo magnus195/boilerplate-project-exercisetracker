d278b9684ef16a3876efb827b962eb4201e0b235
Magnus <magnus.e@outlook.com>
2020-05-13 22:56:50 +0200

    test 1

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track')
 
 app.use(cors())
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(bodyParser.json())
 
 
@@ -20,9 +22,12 @@ app.get('/', (req, res) => {
 
 
 // Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
+/*app.use((req, res, next) => {
+  return next({
+    status: 404,
+    message: 'not found'
+  })
+})*/
 
 // Error Handling middleware
 app.use((err, req, res, next) => {
@@ -41,6 +46,88 @@ app.use((err, req, res, next) => {
   }
   res.status(errCode).type('txt')
     .send(errMessage)
+})
+
+var userSchema = new mongoose.Schema({
+  name: String
+})
+
+var User = mongoose.model('User', userSchema)
+
+var exerciseSchema = new mongoose.Schema({
+  userId: String,
+  description: String,
+  duration: Number,
+  date: Date
+})
+
+var Exercise = mongoose.model('Exercise', exerciseSchema)
+
+app.post('/api/exercise/new-user', (req, res) => {
+  if (req.body.username) {
+    User.create({
+      name: req.body.username
+    }, (err, data) => {
+      if (err) {
+        res.json({
+          "Error": err
+        })
+      } else {
+        res.json(data)
+      }
+    })
+  } else {
+    res.json({
+      "Error": "Invalid input"
+    })
+  }
+})
+
+app.get('/api/exercise/users', (req, res) => {
+  User.find((err, data) => {
+    if (err) {
+      res.json({
+        "Error": err
+      })
+    } else {
+      res.json(data)
+    }
+  })
+})
+
+app.post('/api/exercise/add', (req, res) => {
+  if (!req.body.date) {
+    req.body.date = Date.now()
+  }
+  Exercise.create({
+    userId: req.body.userId,
+    description: req.body.description,
+    duration: req.body.duration,
+    date: req.body.date
+  }, (err, data) => {
+    if (err) {
+      res.json({
+        "Error": err
+      })
+    }
+    res.json(data)
+  })
+})
+
+app.get("/api/exercise/log", (req, res) => {
+  console.log(parseInt(req.query.from))
+  Exercise.find({
+    userId: req.query.userId
+    }
+  ).skip(parseInt(req.query.from)).limit(parseInt(req.query.to)).exec((err, data) => {
+    if (err) {
+      res.json({
+        "Error": err
+      })
+    } else {
+      res.json(data)
+    }
+  })
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
